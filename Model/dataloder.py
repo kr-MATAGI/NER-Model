7f0ab21e5e41a2afe4bcba616a385c4155b19456
kr-MATAGI <dev_matagi@kakao.com>
2022-02-21 20:19:36 +0900

    add train code and dataloader

--- Model/dataloder.py
+++ Model/dataloder.py
@@ -3,16 +3,30 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class ExoBrain_Datasets(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, path: str=""):
+        self.attention_mask = np.load(path+"/attention_mask.npy")
+        self.input_ids = np.load(path+"/input_ids.npy")
+        self.labels = np.load(path+"/labels.npy")
+        self.token_type_ids = np.load(path+"/token_type_ids.npy")
+
+        self.attention_mask = torch.tensor(self.attention_mask)
+        self.input_ids = torch.tensor(self.input_ids)
+        self.labels = torch.tensor(self.input_ids)
+        self.token_type_ids = torch.tensor(self.token_type_ids)
 
     def __len__(self):
-        pass
+        return len(self.attention_mask)
 
-    def __getitem__(self, item):
-        pass
+    def __getitem__(self, idx):
+        items = {
+            "attention_mask": self.attention_mask[idx],
+            "input_ids": self.input_ids[idx],
+            "labels": self.labels[idx],
+            "token_type_ids": self.token_type_ids[idx]
+        }
+
+        return items
 
 ### TEST ###
 if "__main__" == __name__:
     print("[Model.dataloader.py] -----TEST")
-
--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-from transformers import AutoModel
 import logging
 
+from transformers import AutoModel, AdamW, get_linear_schedule_with_warmup
+from torch.utils.data import RandomSampler, DataLoader
+from fastprogress.fastprogress import master_bar, progress_bar
+
+
+
+# train
+def train(model, train_dataset, dev_dataset,
+          batch_size: int=16,
+          learning_rate: int=0.01,):
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size)
+
+    # Prepare optimizer and schedule (linear warmup and decay)
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+         'weight_decay': 0.0},
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+    optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=int(t_total * args.warmup_proportion),
+                                                num_training_steps=t_total)
+
+    global_step = 0
+    tr_loss = 0.0
+
+    model.zero_grad()
+    mb = master_bar(range(int(arg.num_train_epochs)))
+    for epoch in mb:
+        epoch_iterator = progress_bar(train_dataloader, parent=mb)
+        for step, batch in enumerate(epoch_iterator):
+            model.train()
+
+            outputs = model(**batch)
+            loss = outputs[0]
+
+            loss.backwar()
+            tr_loss += loss.item()
+
+            optimizer.step()
+            scheduler.step()
+            model.zero_grad()
+            global_step += 1
 
 ### MAIN ###
 if "__main__" == __name__:
