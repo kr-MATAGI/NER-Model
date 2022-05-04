6d006ff686ae908b8ab89dd4833487f0cfd55ead
kr-MATAGI <dev_matagi@kakao.com>
2022-05-04 23:13:34 +0900

    test bert-lstm

--- custom_model.py
+++ custom_model.py
@@ -1,15 +1,19 @@
 import torch
 import torch.nn as nn
-from transformers import AutoModel
+from transformers import AutoModel, AutoConfig
 
-
+LSTM_HIDDEN_SIZE = 512
 class BERT_LSTM(nn.Module):
     def __init__(self, config):
         super(BERT_LSTM, self).__init__()
+        self.hidd
+
         self.bert = AutoModel.from_pretrained("klue/bert-base", config=config)
 
         ## New Layers
-        self.lstm = nn.LSTM(768, 30, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=config.hidden_size, hidden_size=LSTM_HIDDEN_SIZE//2, num_layers=1,
+                            batch_first=True, bidirectional=True)
+        self.linear = nn.Linear(LSTM_HIDDEN_SIZE, config.num_labels)
 
     def forward(self, input_ids, token_type_ids, attention_mask):
         bert_outputs = self.bert(
@@ -18,11 +22,14 @@ class BERT_LSTM(nn.Module):
             attention_mask=attention_mask
         )
         sequence_output, pooled_output = bert_outputs.last_hidden_state, bert_outputs.pooler_output
-        # sequence_output has the following shape: (batch_size, sequence_length, 768)
-        lstm_output, (h, c) = self.lstm(sequence_output)  ## extract the 1st token's embeddings
-        hidden = torch.cat((lstm_output[:, -1, :256], lstm_output[:, 0, 256:]), dim=-1)
+        lstm_output, hidden = self.lstm(sequence_output)
+        output = self.linear(lstm_output)
 
-        ### assuming that you are only using the output of the last LSTM cell to perform classification
-        #linear_output = self.linear(hidden.view(-1, 256 * 2))
+        return output
 
-        return hidden
+### TEST ###
+if "__main__" == __name__:
+    config = AutoConfig.from_pretrained("klue/bert-base",
+                                        num_labels=30)
+
+    print(config)
