3b9591cbe36e9328e3dfafaac6cd57d7c097a57d
kr-MATAGI <dev_matagi@kakao.com>
2022-05-06 11:24:41 +0900

    d

--- custom_model.py
+++ custom_model.py
@@ -85,14 +85,6 @@ class CRF(nn.Module):
             reduction is ``none``, ``()`` otherwise.
         """
 
-        print(tags.shape)
-        print(emissions.shape)
-        print(tags)
-        print(emissions)
-
-
-        exit()
-
         self._validate(emissions, tags=tags, mask=mask)
         if reduction not in ('none', 'sum', 'mean', 'token_mean'):
             raise ValueError(f'invalid reduction: {reduction}')
@@ -345,10 +337,12 @@ class BERT_LSTM(BertPreTrainedModel):
         super(BERT_LSTM, self).__init__(config)
         self.bert = AutoModel.from_pretrained("klue/bert-base", config=config)
         self.lstm_hidden = 512
+        self.dropout = 0.1
 
         ## New Layers
+        self.dropout = nn.Dropout(self.dropout)
         self.lstm = nn.LSTM(input_size=config.hidden_size, hidden_size=self.lstm_hidden // 2, num_layers=1,
-                            batch_first=True, bidirectional=True)
+                            dropout=self.dropout, batch_first=True, bidirectional=True)
         self.linear = nn.Linear(self.lstm_hidden, config.num_labels)
         self.crf = CRF(num_tags=config.num_labels, batch_first=True)
 
@@ -370,9 +364,6 @@ class BERT_LSTM(BertPreTrainedModel):
         else:
             sequence_of_tags = self.crf.decode(output)
             return sequence_of_tags
-
-
-
 
 ### TEST ###
 if "__main__" == __name__:
