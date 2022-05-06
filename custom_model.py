1dc9eab7633211d050a6206abdb9a8994c049768
kr-MATAGI <dev_matagi@kakao.com>
2022-05-06 15:31:22 +0900

    for test

--- custom_model.py
+++ custom_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from transformers import AutoModel, AutoConfig, BertPreTrainedModel
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 __version__ = '0.7.2'
 
@@ -346,23 +347,33 @@ class BERT_LSTM(BertPreTrainedModel):
         self.linear = nn.Linear(self.lstm_hidden, config.num_labels)
         self.crf = CRF(num_tags=config.num_labels, batch_first=True)
 
-    def forward(self, input_ids, token_type_ids, attention_mask, labels=None):
+    def forward(self, input_ids, token_type_ids, attention_mask, labels=None,
+                pad_id=0, using_pack_sequence=True):
+        seq_len = torch.where(attention_mask == pad_id, 1, 0).tolist()[1].index(True)
+
         bert_outputs = self.bert(
             input_ids=input_ids,
             token_type_ids=token_type_ids,
             attention_mask=attention_mask
         )
         sequence_output, pooled_output = bert_outputs.last_hidden_state, bert_outputs.pooler_output
-        lstm_output, hidden = self.lstm(sequence_output)
-        output = self.linear(lstm_output)
+        sequence_output = self.dropout(sequence_output)
+
+        if using_pack_sequence:
+            pack_padded_output = pack_padded_sequence(sequence_output, seq_len, enforce_sorted=False)
+            lstm_output, hidden = self.lstm(pack_padded_output)
+            lstm_output = pad_packed_sequence(lstm_output, batch_first=True, padding_value=pad_id)
+        else:
+            lstm_output, hidden = self.lstm(sequence_output)
+        emissions = self.linear(lstm_output)
 
         if labels is not None:
-            log_likelihood, sequence_of_tags = self.crf(emissions=output, tags=labels, mask=attention_mask.bool(),
-                                                        reduction="mean"), self.crf.decode(output,
+            log_likelihood, sequence_of_tags = self.crf(emissions=emissions, tags=labels, mask=attention_mask.bool(),
+                                                        reduction="mean"), self.crf.decode(emissions,
                                                                                            mask=attention_mask.bool())
             return log_likelihood, sequence_of_tags
         else:
-            sequence_of_tags = self.crf.decode(output)
+            sequence_of_tags = self.crf.decode(emissions)
             return sequence_of_tags
 
 ### TEST ###
