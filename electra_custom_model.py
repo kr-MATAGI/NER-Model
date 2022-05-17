296cb36d37a1d1ccb0b27203a704d8eee93098dd
kr-MATAGI <dev_matagi@kakao.com>
2022-05-17 12:58:19 +0900

    fix

--- electra_custom_model.py
+++ electra_custom_model.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 from transformers import ElectraModel, ElectraPreTrainedModel, AutoConfig
-from idcnn_layer import IDCNN
 from crf_layer import CRF
 
 #================================================================================================================
