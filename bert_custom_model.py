9557e756e563f0b649313816bee1de02cfb378ae
kr-MATAGI <dev_matagi@kakao.com>
2022-05-18 13:09:46 +0900

    test

--- bert_custom_model.py
+++ bert_custom_model.py
@@ -16,7 +16,7 @@ class BERT_LSTM_CRF(BertPreTrainedModel):
         super(BERT_LSTM_CRF, self).__init__(config)
         self.max_seq_len = 128
 
-        self.bert = AutoModel(config=config)
+        self.bert = AutoModel.from_config(config=config)
         self.lstm_hidden = 512
         self.dropout_rate = 0.1
 
