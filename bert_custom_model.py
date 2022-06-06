8f93e4a3311d15e0342e9be1b752653d4da70534
kr-MATAGI <dev_matagi@kakao.com>
2022-06-06 14:52:35 +0900

    add, bert(pos info) + crf, bert(pos info) + lstm + crf

--- bert_custom_model.py
+++ bert_custom_model.py
@@ -137,11 +137,12 @@ class BERT_POS_LSTM(BertPreTrainedModel):
                 Use from_pretrained() to load the model weights.
         '''
         self.bert = AutoModel.from_config(config=config)
-        self.lstm = nn.LSTM(input_size=config.hidden_size + (self.pos_embed_out_dim * 3),
-                            hidden_size=config.hidden_size + (self.pos_embed_out_dim * 3),
-                            num_layers=1, batch_first=True, dropout=0.3)
+        # self.lstm = nn.LSTM(input_size=config.hidden_size + (self.pos_embed_out_dim * 3),
+        #                     hidden_size=config.hidden_size + (self.pos_embed_out_dim * 3),
+        #                     num_layers=1, batch_first=True, dropout=0.3)
         self.dropout = nn.Dropout(0.3)
         self.classifier = nn.Linear(config.hidden_size + (self.pos_embed_out_dim * 3), config.num_labels)
+        self.crf = CRF(num_tags=config.num_labels, batch_first=True)
 
         self.post_init()
 
@@ -163,9 +164,18 @@ class BERT_POS_LSTM(BertPreTrainedModel):
         sequence_output = outputs[0] # [batch_size, seq_len, hidden_size]
         concat_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
         concat_embed = torch.concat([sequence_output, concat_embed], dim=-1)
-        lstm_out, _ = self.lstm(concat_embed) # [batch_size, seq_len, hidden_size]
-        lstm_out = self.dropout(lstm_out)
+        # lstm_out, _ = self.lstm(concat_embed) # [batch_size, seq_len, hidden_size]
+        lstm_out = self.dropout(concat_embed)
         logits = self.classifier(lstm_out)
+
+        # crf
+        if labels is not None:
+            log_likelihood, sequence_of_tags = self.crf(emissions=logits, tags=labels, mask=attention_mask.bool(),
+                                                        reduction="mean"), self.crf.decode(logits, mask=attention_mask.bool())
+            return log_likelihood, sequence_of_tags
+        else:
+            sequence_of_tags = self.crf.decode(logits)
+            return sequence_of_tags
 
         loss = None
         if labels is not None:
