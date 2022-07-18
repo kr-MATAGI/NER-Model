03761b7627f88679688827d0cdddbca2bb1954a4
kr-MATAGI <dev_matagi@kakao.com>
2022-07-18 12:15:56 +0900

    사용자 정한 세분류가 포함된 샘플 추출

--- extract_sample.py
+++ extract_sample.py
@@ -2,8 +2,9 @@ import os
 import json
 import copy
 import pandas as pd
+from openpyxl import Workbook
 
-from typing import List
+from typing import List, Dict
 from dataclasses import dataclass, field
 
 ### Data Structure
@@ -67,9 +68,80 @@ def extract_ne_samples_from_json(target_path: str, target_ne_list: List[str]):
     return ret_sample_list
 
 #===============================================================
-def make_excel_file_using_extarct_results(data_list: List[Sample_Data]):
+def make_target_ne_dict_extract_results(data_list: List[Sample_Data], target_ne_list: List[str]):
 #===============================================================
-    pass
+    # init
+    ne_data_dict = {target_ne: [] for target_ne in target_ne_list}
+
+    # arrange
+    for sample_data in data_list:
+        is_insert = False
+        for ne_data in sample_data.ne_list:
+            if is_insert:
+                break
+            for target_ne in target_ne_list:
+                if "*" in target_ne:
+                    lhs_target_ne = target_ne.split("_")[0]
+                    if lhs_target_ne in ne_data.label:
+                        ne_data_dict[target_ne].append(sample_data)
+                        is_insert = True
+                        break
+                else:
+                    if target_ne == ne_data.label:
+                        ne_data_dict[target_ne].append(sample_data)
+                        is_insert = True
+                        break
+
+    # print (key, values) size
+    dict_values_size = 0
+    for target_ne in target_ne_list:
+        print(f"[extract_sample][make_excel_file_using_extract_results] {target_ne}.size: {len(ne_data_dict[target_ne])}")
+        dict_values_size += len(ne_data_dict[target_ne])
+    print(f"[extract_sample][make_excel_file_using_extract_results] total dict.values.size {dict_values_size}")
+
+    return ne_data_dict
+
+#===============================================================
+def make_excel_file_using_extract_results(target_ne_dict: Dict[str, List[Sample_Data]]):
+#===============================================================
+    # create sheet
+    new_wb = Workbook()
+    work_sheet = new_wb.active
+
+    for dic_idx, (key, sample_list) in enumerate(target_ne_dict.items()):
+        print(f"[extract_sample][make_excel_file_using_extract_results] {key} is Processing... (size: {len(sample_list)})")
+        if key != work_sheet.title:
+            if "*" in key:
+                work_sheet.title = key.replace("*", "all")
+            else:
+                work_sheet.title = key
+
+        row = 2
+        col = 2
+        work_sheet.cell(row, col, value="id")
+        work_sheet.cell(row, col+1, value="sentence")
+        work_sheet.cell(row, col+2, value="ne_text")
+        work_sheet.cell(row, col+3, value="ne_label")
+
+        row += 1
+        for sp_idx, sample_data in enumerate(sample_list):
+            work_sheet.cell(row, col, value=sample_data.id)
+            work_sheet.cell(row, col+1, value=sample_data.sent)
+
+            for ne_item in sample_data.ne_list:
+                work_sheet.cell(row, col+2, ne_item.text)
+                work_sheet.cell(row, col+3, ne_item.label)
+                row += 1
+            row += 1
+
+        # new sheet
+        if dic_idx != len(target_ne_dict.keys())-1:
+            work_sheet = new_wb.create_sheet()
+
+    # save *.xlsx
+    save_path = "./target_samples.xlsx"
+    new_wb.save(save_path)
+    print(f"[extract_sample][make_excel_file_using_extract_results] Complete - {save_path}")
 
 ### Main
 if "__main__" == __name__:
@@ -84,6 +156,7 @@ if "__main__" == __name__:
                                  "LC_*",
                                  "QT_*",
                                  "CV_OCCUPATION"]
+    print(f"[extract_sample][__main__] {target_ne_list}")
 
     # load
     target_dir_path = "./data/old_nikl"
@@ -102,5 +175,9 @@ if "__main__" == __name__:
         all_sample_data_list.extend(extract_data_list)
     print(f"[extract_sample][__main__] all sample data size: {len(all_sample_data_list)}")
 
+    # make target_ne_dict
+    target_ne_dict = make_target_ne_dict_extract_results(data_list=all_sample_data_list,
+                                                         target_ne_list=target_ne_list)
+
     # make excel
-    make_excel_file_using_extarct_results(all_sample_data_list)
+    make_excel_file_using_extract_results(target_ne_dict)
