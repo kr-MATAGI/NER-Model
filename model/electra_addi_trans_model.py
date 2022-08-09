66643f880e0b51072542968d1caf5365b3eb616a
kr-MATAGI <dev_matagi@kakao.com>
2022-08-10 03:55:46 +0900

    add, lstm test

--- model/electra_addi_trans_model.py
+++ model/electra_addi_trans_model.py
@@ -35,7 +35,7 @@ class Electra_Addi_Feature_Model(ElectraPreTrainedModel):
         self.dropout_rate = 0.1
 
         # for Transformer Encoder Config
-        self.d_model_size = config.hidden_size + (self.pos_embed_dim * 3) + self.max_eojeol_len
+        self.d_model_size = config.hidden_size + (self.pos_embed_dim * 3) + self.entity_embed_dim
         self.t_enc_config = copy.deepcopy(config)
         self.t_enc_config.num_hidden_layers = 4
         self.t_enc_config.hidden_size = self.d_model_size
@@ -54,10 +54,12 @@ class Electra_Addi_Feature_Model(ElectraPreTrainedModel):
         self.pos_embedding_3 = nn.Embedding(self.num_pos_labels, self.pos_embed_dim)
 
         # Eojeol Boundary Embedding
-        self.eojeol_embedding = nn.Embedding(self.max_seq_len, self.max_eojeol_len)
+        # self.eojeol_embedding = nn.Embedding(self.max_seq_len, self.max_eojeol_len)
 
         # Entity Embedding
         # self.entity_embedding = nn.Embedding(self.max_seq_len, self.entity_embed_dim)
+
+        self.eojeol_entity_embedding = nn.Embedding(self.max_seq_len, self.entity_embed_dim)
 
         # FFN_1
         # self.ffn_1 = nn.Linear()
@@ -73,6 +75,23 @@ class Electra_Addi_Feature_Model(ElectraPreTrainedModel):
 
         # Initialize weights and apply final processing
         self.post_init()
+
+    #===================================
+    def _make_eojeol_entity_embed(self, eojeol_ids, entity_ids):
+    #===================================
+        batch_size, seq_len = eojeol_ids.size() # [64, 128]
+
+        device = eojeol_ids.device
+        eojeol_ids = eojeol_ids.detach().cpu()
+        entity_ids = entity_ids.detach().cpu()
+        new_tensor = torch.zeros(batch_size, seq_len, dtype=torch.long)
+        for batch_idx in range(batch_size):
+            for idx, (eojeol, entity) in enumerate(zip(eojeol_ids[batch_idx], entity_ids[batch_idx])):
+                if 0 != eojeol:
+                    new_tensor[batch_idx][idx] = 1
+                if 0 != entity:
+                    new_tensor[batch_idx][idx] = 2
+        return new_tensor.to(device)
 
     #===================================
     def forward(
@@ -101,15 +120,17 @@ class Electra_Addi_Feature_Model(ElectraPreTrainedModel):
 
         # Eojeol
         # [batch_size, max_seq_len, max_eojeol_len]
-        eojeol_embed = self.eojeol_embedding(eojeol_ids)
+        # eojeol_embed = self.eojeol_embedding(eojeol_ids)
 
         # Entity
         # entity_embed = self.entity_embedding(entity_ids)
+        eojeol_entity_tensor = self._make_eojeol_entity_embed(eojeol_ids=eojeol_ids, entity_ids=entity_ids)
+        eojeol_entity_embed = self.eojeol_entity_embedding(eojeol_entity_tensor)
 
         # All Features Concat
         concat_pos_embed = torch.concat([pos_embed_1, pos_embed_2, pos_embed_3], dim=-1)
         # [64, 128, 1202], [64, 128, 1328]
-        concat_all_embed = torch.concat([plm_last_hidden, concat_pos_embed, eojeol_embed], dim=-1)
+        concat_all_embed = torch.concat([plm_last_hidden, concat_pos_embed, eojeol_entity_embed], dim=-1)
 
         # Transformer Encoder
         extend_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
